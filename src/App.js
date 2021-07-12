1c2899235e0c0043ff85b45a77cea7f3c9c7390a
KiwiTech <kiwitech@KiwiTechs-Mac-mini.local>
2021-07-12 18:08:47 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Container } from '@material-ui/core';
+import SimpleBottonNavigation from './components/MainNav';
+import Header from './components/header/Header';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Trending from './Pages/Trending/Trending';
+import Movies from './Pages/Movies/Movies';
+import Search from './Pages/Search/Search';
+import Series from './Pages/Series/Series';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className="app">
+        <Container>
+        <Switch>
+            <Route path="/" component={Trending} exact />
+            <Route path="/movies" component={Movies} />
+            <Route path="/series" component={Series} />
+            <Route path="/search" component={Search} />
+        </Switch>
+        </Container>
+      </div>
+      <SimpleBottonNavigation />
+    </BrowserRouter>
   );
 }
 
